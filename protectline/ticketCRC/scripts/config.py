2c331f8ac0419a55b24c7c36e221ff2e39afb8a2
arthurlormeauline <arthur.lormeau@protectline.fr>
2025-10-16 14:21:47 +0200

    delete ticket crc

--- protectline/ticketCRC/scripts/config.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-SCRIPTS_DIR = Path(__file__).parent
-ROOT_DIR = SCRIPTS_DIR.parent
-
-INPUT_DIR = ROOT_DIR / 'input'
-OUTPUT_DIR = ROOT_DIR / 'output'
-TEMP_DIR = ROOT_DIR / 'temp'
-BATCHES_DIR = TEMP_DIR / 'batches'
